978abc6296a45a8404465dd6384f02754093141c
Leo Neto <oleoneto@gmail.com>
2022-03-13 21:31:36 -0400

    feat: greet multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -34,3 +34,20 @@ func Hello(name string) (string, error) {
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
+
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
--- hello/hello.go
+++ hello/hello.go
@@ -21,11 +21,25 @@ func greetPerson(name string) {
 	fmt.Println(message)
 }
 
+
+func greetPeople(names []string) {
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, name := range names {
+		fmt.Println(messages[name])
+	}
+}
+
+
 func main() {
 	// Prepare Logger
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0) // suppresses the time, source file, and line number
 
-	// printQuote()
-	greetPerson("Leo")
+	names := []string{"Leo", "John", "Oliver"}
+	greetPeople(names)
 }
